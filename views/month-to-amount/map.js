c73e6eecda0a27189cb3c990326a094915c82018
crccheck <c@crccheck.com>
2010-11-27 21:37:48 -0600

    change month-to-amount view to allow for 2nd level detail

--- views/month-to-amount/map.js
+++ views/month-to-amount/map.js
@@ -1,8 +1,12 @@
 function(doc) {
-  if (doc.place && doc.date) {
+  if (doc.place && doc.date && doc.amount) {
     var date = new Date(doc.date);
     var month = date.getMonth();
     if (month < 10) month = '0' + month;
-    emit(date.getYear() +""+ month, +doc.amount);
+    date = date.getYear() +""+ month;
+
+    var name = doc.place.namefix || doc.place.name.toLowerCase().replace(/[^a-z0-9 ]/g,'').replace(/^\s*(the)?\s*|\s+$/g, '').replace(/\s{2,}/g,' ');
+
+    emit([+date, name], +doc.amount);
   }
 }
