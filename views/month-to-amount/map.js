9d841d35bba4c247bf4e0d1f0ea2469c439fca7d
crccheck <c@crccheck.com>
2010-11-29 00:30:02 -0600

    color by type of expenditure
    todo there is a bug in some of the treemaps where it's not filling the
    parent block with this change.

--- views/month-to-amount/map.js
+++ views/month-to-amount/map.js
@@ -1,12 +1,16 @@
 function(doc) {
-  if (doc.place && doc.date && doc.amount) {
+  if (doc.date && doc.amount) {
     var date = new Date(doc.date);
     var month = date.getMonth();
     if (month < 10) month = '0' + month;
     date = date.getYear() +""+ month;
 
-    var name = doc.place.namefix || doc.place.name.toLowerCase().replace(/[^a-z0-9 ]/g,'').replace(/^\s*(the)?\s*|\s+$/g, '').replace(/\s{2,}/g,' ');
-
-    emit([+date, name], +doc.amount);
+    var name = '';
+    if (doc.place){
+        name = doc.place.namefix || doc.place.name.toLowerCase().replace(/[^a-z0-9 ]/g,'').replace(/^\s*(the)?\s*|\s+$/g, '').replace(/\s{2,}/g,' ');
+    } else {
+        name = doc[doc.type];
+    }
+    emit([+date, doc.type, name], +doc.amount);
   }
 }
