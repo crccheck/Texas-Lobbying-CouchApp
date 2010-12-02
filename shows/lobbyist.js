6b2cb1aaf1faf7a5b3f5b62210cb84d25027d0e6
crccheck <c@crccheck.com>
2010-12-01 19:06:12 -0600

    refine show function

--- shows/lobbyist.js
+++ shows/lobbyist.js
@@ -1,5 +1,27 @@
 function(doc, req){
+    if (doc) {
+    var body = '';
+    body += '<h1>' + doc.lobbyist.name + '</h1>'
+        + "<h2>Clients</h2>";
+    + '<ul>';
+    doc.client.forEach(function(client){
+        body += '<li>'+client.name
+        + client.compensation.type
+        + ' $' + client.compensation.low + ' to $'
+        + client.compensation.high
+        +'</li>';
+    });
+    body += '</ul>';
+    delete doc._revisions;
+    delete doc._id;
+    delete doc._rev;
+    body += '<pre>' + JSON.stringify(doc) + '</pre>';
     return {
-        body: JSON.stringify(doc)
+        body: body
+    }
+    } else {
+        return {
+            body: 'Does Not Exist'
+        }
     }
 }
