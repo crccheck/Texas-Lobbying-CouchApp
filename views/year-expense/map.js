773ef24d58432b88e52149efd796f1967525ed43
crccheck <c@crccheck.com>
2010-12-06 16:49:55 -0600

    year to expense view for generating the lobbyist index

---
+++ views/year-expense/map.js
@@ -0,0 +1,17 @@
+function(doc) {
+  if (doc.type == 'cover'){
+    var year = doc.report.year || 0;
+    for (var type in doc.benefited){
+      var amount;
+      if (amount = doc.benefited[type]){
+          emit([+year, doc.lobbyist.id, 2, type], +amount);
+        }
+    }
+    for (var expenseType in doc.expenses){
+      var expenseAmount;
+      if (expenseAmount = doc.expenses[expenseType]){
+          emit([+year, doc.lobbyist.id, 1, expenseType], +expenseAmount);
+        }
+    }
+  }
+}
---
+++ views/year-expense/reduce.js
@@ -0,0 +1,3 @@
+function(keys, values){
+  return Math.round(sum(values)*100)/100;
+}
