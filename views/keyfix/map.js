1f0223b43516793ec62fab850e3c0ee1e3c1b59b
crccheck <c@crccheck.com>
2010-12-09 14:47:10 -0600

    fix keyfix map.js was overzealous

--- views/keyfix/map.js
+++ views/keyfix/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  if (doc.place.namefix) {
+  if (doc.place && doc.place.namefix) {
     var name = doc.place.name.toLowerCase().replace(/^\s*(the)?\s*| +$/g, '').replace(/[^a-z0-9 ]/g, '').replace(/\s{2,}/g,' ');
     emit(name, doc.place.namefix);
   }
