be4fafffd5678df42b5fd249ae862297567d3a31
crccheck <c@crccheck.com>
2010-12-13 14:38:05 -0600

    new view: client-to-lobbyist

---
+++ views/client-to-lobbyist/map.js
@@ -0,0 +1,17 @@
+function slugify(text){
+    text = text.toLowerCase().replace(/[^-a-z0-9,&\s]+/g, '');
+    text = text.replace(/-/g, "_");
+    text = text.replace(/\s/g, "-");
+    return text;
+}
+function(doc) {
+    if (doc.type == 'lobbyist'){
+        for each (var client in doc.client){
+            if (client.name){
+                var key = [slugify(client.name), doc.year];
+                var value = doc.lobbyist.id;
+                emit(key, value);
+            }
+        }
+    }
+}
