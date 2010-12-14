ac628c5faa544bd058206648a3232b91f0e95291
crccheck <c@crccheck.com>
2010-12-14 14:29:34 -0600

    update slugify function

--- views/client-to-compensation/map.js
+++ views/client-to-compensation/map.js
@@ -1,7 +1,7 @@
 function slugify(text){
     text = text.toLowerCase().replace(/[^-a-z0-9\s]+/g, '');
-    text = text.replace(/-/g, "_");
-    text = text.replace(/\s/g, "-");
+    text = text.replace(/-+/g, "_");
+    text = text.replace(/\s+/g, "-");
     return text;
 }
 function(doc) {
--- views/year_client-compensation/map.js
+++ views/year_client-compensation/map.js
@@ -1,9 +1,10 @@
 function slugify(text){
     text = text.toLowerCase().replace(/[^-a-z0-9\s]+/g, '');
-    text = text.replace(/-/g, "_");
-    text = text.replace(/\s/g, "-");
+    text = text.replace(/-+/g, "_");
+    text = text.replace(/\s+/g, "-");
     return text;
 }
+
 function(doc) {
     if (doc.type == 'lobbyist'){
         for each (var client in doc.client){
