db2487641a352ba0f6b3f31dc94d2b1a7817f12b
crccheck <c@crccheck.com>
2010-12-16 17:04:07 -0600

    fix client view

--- views/client/reduce.js
+++ views/client/reduce.js
@@ -2,7 +2,7 @@ function (keys, values){
     o = [0, ''];
     values.forEach(function(x){
         o[0] += x[0];
-        o[1]  = x[1];
+        if (x[1]) o[1] = x[1];
         o[2]  = x[2];
     });
     return o;
